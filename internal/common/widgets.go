c134061b1fc2c0417a2849825472f7acad8d8da8
Harpuia <zetsubou.harpuia@gmail.com>
2025-12-07 20:03:43 -0300

    ref: se mueven inicializacion de componentes de la GUI a su propio paquete

--- internal/common/widgets.go
+++ internal/common/widgets.go
@@ -1,10 +1,24 @@
 package common
 
 import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 var ConnectButton *widget.Button
+var PitchSlider *widget.Slider
+var PitchRow *fyne.Container
+
+// Internal
+var leftSpacer *canvas.Rectangle
+var left *fyne.Container
+var pitchLabel *canvas.Text
 
 func InitConnectButton(onClick func()) {
 	ConnectButton = widget.NewButton("Connect to Twitch", onClick)
@@ -13,4 +27,27 @@ func InitConnectButton(onClick func()) {
 func SetConnected() {
 	ConnectButton.SetText("Connected")
 	ConnectButton.Disable()
+}
+
+func initPitchSlider(pitchData binding.Float) {
+	PitchSlider = widget.NewSliderWithData(0.2, 2.0, pitchData)
+	PitchSlider.Step = 0.1
+	PitchSlider.Resize(fyne.NewSize(500, 200))
+}
+
+func initLeftPitchLabel() {
+	leftSpacer = canvas.NewRectangle(color.Transparent)
+	leftSpacer.SetMinSize(fyne.NewSize(20, 0))
+	pitchLabel = canvas.NewText("Voice Pitch", color.White)
+	left = container.NewHBox(leftSpacer, pitchLabel)
+	PitchRow = container.New(
+		layout.NewBorderLayout(nil, nil, left, nil),
+		left,
+		PitchSlider,
+	)
+}
+
+func InitPitchRow(pitchData binding.Float) {
+	initPitchSlider(pitchData)
+	initLeftPitchLabel()
 }
--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"bytes"
-	"image/color"
 	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
 	"github.com/faiface/beep/wav"
 
 	"ac-tts/internal/animalese"
@@ -48,30 +44,14 @@ func main() {
 	defer streamer.Close()
 	reproductor.InitSpeaker(format)
 
-	message := widget.NewLabel("Estado")
-	message.SetText("TTS Iniciado")
-
 	pitchData := binding.BindFloat(&common.Pitch)
-	pitchSlider := widget.NewSliderWithData(0.2, 2.0, pitchData)
-	pitchSlider.Step = 0.1
-	pitchSlider.Resize(fyne.NewSize(500, 200))
-	leftSpacer := canvas.NewRectangle(color.Transparent)
-	leftSpacer.SetMinSize(fyne.NewSize(20, 0))
+	common.InitPitchRow(pitchData)
 
-	pitchLabel := canvas.NewText("Voice Pitch", color.White)
-
-	left := container.NewHBox(leftSpacer, pitchLabel)
-
-	pitchRow := container.New(
-		layout.NewBorderLayout(nil, nil, left, nil),
-		left,
-		pitchSlider,
-	)
 	common.InitConnectButton(func() { twitch.GetAuthorization() })
 	w.Resize(fyne.NewSize(400, 400))
 	w.SetContent(
 		container.NewVBox(
-			pitchRow,
+			common.PitchRow,
 			container.NewCenter(common.ConnectButton),
 		),
 	)
