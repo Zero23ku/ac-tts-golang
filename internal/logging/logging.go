2e68a45ccecc4116b16a7de70acf7bdcdd3bbb5d
Harpuia <zetsubou.harpuia@gmail.com>
2025-12-08 19:47:53 -0300

    feat: se añaden los errores al final del archivo si este ya tiene información

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -9,6 +9,7 @@ import (
 var logName = "error-"
 var extension = ".txt"
 var timeFormat = "02-01-2006-15:04:05"
+var newLine = "\n"
 
 func CreateLog(err error) {
 	t := time.Now()
@@ -16,6 +17,17 @@ func CreateLog(err error) {
 	fileName := logName + tFormat + extension
 
 	path := filepath.Join(os.TempDir(), fileName)
-	os.WriteFile(path, []byte(err.Error()), 0644)
+	file, openErr := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if openErr != nil {
+		//Todo se derrumbó, dentro de mi, dentro de mi
+		panic(openErr)
+	}
+	defer file.Close()
+
+	log := tFormat + newLine + err.Error() + newLine
+	if _, writeErr := file.WriteString(log); writeErr != nil {
+		//WHAT IS HAPPENING AAAAA
+		panic(writeErr)
+	}
 
 }
